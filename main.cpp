6897fc7f5e53fb52448c4b96c4e2518c52327b88
DESKTOP-FNCULJ1\YuHeng Hsieh <scrashedward@gmail.com>
2017-12-14 03:16:12 +0800

    Add evaluate function

--- main.cpp
+++ main.cpp
@@ -3,18 +3,21 @@
 #include <ctime>        // std::time
 #include <cstdlib>      // std::rand, std::srand
 #include <climits>
+#include <iostream>
+#include <unordered_set>
 #include "myrand.h"
 
-#define N 100 // population size 
+#define N 10000 // population size 
 
 using namespace std;
 void getIndexFromMatchId(int*, int*, int, int);
 void exitWithError(int);
+int evaluate(int*);
 
 MyRand myrand;
 
 // 10 time slots per week
-int nWeek = 8;
+int nWeek = 4;
 int nTeam = 10;
 int nField = 2;
 int nTimeSlot = nWeek * 10 * 2;
@@ -46,9 +49,30 @@ int main()
 	for (int i = 0; i < N; i++)
 	{
 		chromosomes[i] = new int[l];
-		myrand.uniformArray(chromosomes[i], 80, 0, 79);
+		myrand.uniformArray(chromosomes[i], l, 0, l-1);
 	}
 
+	int a, b;
+
+	getIndexFromMatchId(&a, &b, 12, 10);
+
+	cout << a << " " << b << endl;
+
+	int temp[20] = {
+		0, 45, 45, 45,
+		1, 45, 45, 45,
+		2, 45, 45, 45,
+		45, 45, 45, 45,
+		45, 45, 45, 45,
+	};
+
+	int good = 0, bad = 0;
+	for (int i = 0; i < N; ++i)
+	{
+		if (evaluate(chromosomes[i]) == INT_MIN) bad++;
+		else good++;
+	}
+	cout << "good = " << good << ", bad = " << bad << endl;
 
 
 
@@ -97,10 +121,58 @@ int evaluate(int* chromosome)
 
 	for (int w = 0; w < nWeek; ++w)
 	{
-		for (int t = 0; t < 10; ++t)
+		unordered_set<int> b2bSet, b2bSetLastDay;
+		for (int d = 0; d < 5; ++d) // 5 days per week
 		{
-			
+			int base = (w * 20 + d * 4);
+			int t1, t2, t3, t4;
+			for (int i = 0; i < 4; ++i)
+			{
+				if (chromosome[base + i] >= nMatch) continue;
+				getIndexFromMatchId(&t1, &t2, chromosome[base + i], nTeam);
+
+				fitness += preferenceArray[t1][d * 2 + i / 2];
+				fitness += preferenceArray[t2][d * 2 + i / 2];
+
+				for (int j = i + 1; j < 4; ++j) 
+				{
+					if (chromosome[base + j] >= nMatch) continue;
+					getIndexFromMatchId(&t3, &t4, chromosome[base + j], nTeam);
+					// do not allow game from the same day
+					if (t1 == t3 || t1 == t4 || t2 == t3 || t2 == t4) return INT_MIN;
+				}
+
+				for (int dComp = d + 1; dComp < d + 3 && dComp < 5; ++dComp)
+				{
+					for (int k = 0; k < 4; ++k)
+					{
+						if (chromosome[w * 20 + dComp * 4 + k] >= nMatch) continue;
+						getIndexFromMatchId(&t3, &t4, chromosome[w * 20 + dComp * 4 + k], nTeam);
+						if (t1 == t3 || t1 == t4 || t2 == t3 || t2 == t4)
+						{
+							if (dComp - d == 1)
+							{
+								fitness -= 3;
+								if (t1 == t3 || t1 == t4) 
+								{
+									if (b2bSetLastDay.find(t1) != b2bSetLastDay.end()) return INT_MIN;
+									else b2bSet.insert(t1);
+								}
+								if (t2 == t3 || t2 == t4)
+								{
+									if (b2bSetLastDay.find(t2) != b2bSetLastDay.end()) return INT_MIN;
+									else b2bSet.insert(t2);
+								}
+							}
+							else if (dComp - d == 2) fitness -= 1;
+						}
+					}
+				}
+			}
+			b2bSetLastDay = b2bSet;
+			b2bSet.clear();
 		}
 	}
 
+	return fitness;
 }
